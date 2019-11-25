9a4b6ad3863f8276bdb0ba12d591c95a7b7b2d5b
Philipp Dubach <philipp.dubach@axonlab.ch>
2019-11-25 20:57:56 +0100

    add dockerhub publish script

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker login --username=dubachphil
+docker push dubachphil/hadoop_base:latest
+docker push dubachphil/hadoop_namenode:latest
+docker push dubachphil/hadoop_datanode:latest
+docker push dubachphil/hadoop_resourcemanager:latest
+docker push dubachphil/hadoop_historyserver:latest
+docker push dubachphil/hadoop_nodemanager:latest
+docker push dubachphil/spark_base:latest
+docker push dubachphil/spark_master:latest
+docker push dubachphil/spark_worker:latest
+
